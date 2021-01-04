9cfec74a79fbe499f38ca9796fe6156885666ebf
pdelpriore <paul.del.priore29@gmail.com>
2021-01-04 15:43:25 +0100

    add new ml model and correct features encoding

--- predict/methods/encoding.py
+++ predict/methods/encoding.py
@@ -6,7 +6,7 @@ import os
 def encode_features(X):
     encoder = pd.read_pickle(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'ml/mushrooms_feature_encoder.pickle'))
 
-    X_np_array = np.array(X).reshape(1, 20)
+    X_np_array = np.array(X).reshape(1, 12)
 
     X_encoded = encoder.transform(X_np_array)
 
